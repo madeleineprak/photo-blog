9a6b888a4bb7521953a5b104a00767fafc7faaec
Madeleine Prak <madeleineprak@gmail.com>
2020-03-15 21:52:17 -0700

    add cursor, footer

---
+++ src/components/Footer/index.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function Footer() {
+    return (
+        <div id="contact" className="footer">
+            <div className="has-text-centered">
+                <p>
+                Cursor icon made by <a href="https://www.flaticon.com/authors/freepik" title="Freepik">Freepik</a> from <a href="https://www.flaticon.com/" title="Flaticon"> www.flaticon.com</a>
+                </p>
+            </div>
+        </div>
+    )
+}
+
+export default Footer;
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,12 +1,16 @@
 import React from 'react'
 import Nav from '../components/Nav'
 import Gallery from '../components/Gallery'
+import Footer from '../components/Footer'
 
 function Home() {
     return (
         <div>
             <Nav/>
             <Gallery/>
+            <br></br>
+            <br></br>
+            <Footer/>
         </div>
         
     )
